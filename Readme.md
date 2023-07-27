7adbe5f706cf2db5ae9dc7cfc592cab30abc4809
Uttam Gupta <uttamgupta.zenmonk@outlook.com>
2023-07-27 18:07:26 +0530

    Redis And Express Session

--- Readme.md
+++ Readme.md
@@ -4,46 +4,76 @@
 
 ### KEYS
 
-### `SET keyname data`
--   set key value
-### `GET keyname`
--   get key value
-### `TTL keyname`
--   expiring time of key (-1 means never||-2 means already deleted)
-### `EXPIRE keyname timeinsec`
--   set time after which key expires
-### `KEYS *`
-- shows all key
-### `FLUSHALL`
-- clears out redis
+#### `SET keyname data`
+
+set key value
+
+#### `GET keyname`
+
+get key value
+
+#### `TTL keyname`
+
+expiring time of key (-1 means never||-2 means already deleted)
+
+#### `EXPIRE keyname timeinsec`
+
+set time after which key expires
+
+#### `KEYS *`
+
+shows all key
+
+#### `FLUSHALL`
+
+clears out redis
 
 ### ARRAY
 
-### `LPUSH varname data`
--   push in array
-### `LRANGE varname startindex endindex(-1 for all)`
--   get values in array
-### `RPOP varname`
--   remove top value in array
+#### `LPUSH varname data`
+
+push in array
+
+#### `LRANGE varname startindex endindex(-1 for all)`
+
+get values in array
+
+#### `RPOP varname`
+
+remove top value in array
 
 ### SET
 
-### `SADD varname data`
--   push in set
-### `SMEMBERS varname`
--   get values in set
-### `SREM varname data`
--   remove in array
+#### `SADD varname data`
+
+push in set
+
+#### `SMEMBERS varname`
+
+get values in set
+
+#### `SREM varname data`
+
+remove in array
 
 ### HASMAP
 
-### `HSET varname keyname data`
--   set key and data in hashmap
-### `HGET varname keyname`
--   get key and data in hashmap
-### `HGETALL varname`
--   get all key and data in hashmap
-### `HDEL varname keyname`
--   delete key and data in hashmap
-### `HEXISTS varname keyname`
--   checks for key and data in hashmap
+#### `HSET varname keyname data`
+
+set key and data in hashmap
+
+#### `HGET varname keyname`
+
+get key and data in hashmap
+
+#### `HGETALL varname`
+
+get all key and data in hashmap
+
+#### `HDEL varname keyname`
+
+delete key and data in hashmap
+
+#### `HEXISTS varname keyname`
+
+checks for key and data in hashmap
--- index.js
+++ index.js
@@ -5,44 +5,30 @@ const redis = require("redis");
 const axios = require("axios");
 const client = redis.createClient();
 const ExpireTime = 3600
+const path = require("path");
 const session = require("express-session");
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-const day=24*60*60*1000
+const day = 24 * 60 * 60 * 1000
 app.use(session({
     secret: "secretkey",
     resave: true,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         maxAge: day,
     }
 }));
 app.use(express.json());
+app.use(express.static("./pages"));
+app.use('/assets', express.static(path.join(__dirname, "/assets")));
 app.use(express.text());
 (async () => {
     await client.on("error", (error) => console.error(error));
     await client.connect();
     console.log("redis client connected");
-
 })();
-
-app.get("/", (req, res) => {
-    res.send("WORKING");
-})
-
 // checking redis usage
 app.get("/data", async (req, res) => {
-    // console.log("there");
-    // const data = await client.get("photo")
-    // if (data) {
-    //     console.log("cache");
-    //     res.json(JSON.parse(data))
-    // } else {
-    //     console.log("api");
-    //     const { data } = await axios.get("https://jsonplaceholder.typicode.com/photos");
-    //     client.setEx("photo", ExpireTime, JSON.stringify(data));
-    //     res.json(data)
-    // }
     const response = await redisGetOrSetData("photo", async () => {
         const { data } = await axios.get("https://jsonplaceholder.typicode.com/photos");
         return data
@@ -54,14 +40,10 @@ app.get("/data", async (req, res) => {
     }
 })
 app.get("/data/:id", async (req, res) => {
-    // const id = req.params.id;
-    // console.log(id);
     const response = await redisGetOrSetData(`photo${req.params.id}`, async () => {
         const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`);
         return data
     })
-    // const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`);
-    // res.json(data)
     if (req.session.authorized) {
         res.json(response)
     } else {
@@ -89,15 +71,55 @@ const redisGetOrSetData = async (key, callBack) => {
 
 // login api to check session
 
-app.get("/login", async (req, res) => {
-    req.session.authorized = true
-    res.send("Logged in")
-})
-app.get("/logout", async (req, res) => {
-    req.session.destroy()
-    res.send("Logged out")
-})
+app.get("/", async (req, res) => {
+    if (req.session.authorized) {
+        res.redirect("/homepage")
+    } else {
+        res.sendFile(path.join(__dirname, "./pages/login.html"))
 
+    }
+})
+app.get("/register", async (req, res) => {
+    if (req.session.authorized) {
+        res.redirect("/homepage")
+    } else {
+        res.sendFile(path.join(__dirname, "./pages/register.html"))
+    }
+})
+app.get("/homepage", async (req, res) => {
+    if (req.session.authorized) {
+        res.sendFile(path.join(__dirname, "./pages/homepage.html"))
+    } else {
+        res.redirect("/")
+    }
+})
+app.post("/register", async (req, res) => {
+    if (req.body.username && req.body.password) {
+        await client.set(req.body.username, req.body.password)
+        res.redirect("/")
+    } else {
+        res.redirect("/register")
+    }
+})
+app.post("/", async (req, res) => {
+    if (req.body.username && req.body.password) {
+        const response = await client.get(req.body.username)
+        if (response === req.body.password) {
+            req.session.user = req.body
+            req.session.authorized = true
+            res.redirect("/homepage")
+        } else {
+            res.redirect("/")
+        }
+    } else {
+        res.redirect("/")
+    }
+
+})
+app.post("/logout", async (req, res) => {
+    req.session.destroy()
+    res.redirect("/")
+})
 app.listen(1000, () => {
     console.log("WORKING ON", 1000);
 })
